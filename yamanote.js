e1bdca43026fb6a707956e61a6354db0a05462c8
MMirai <g121749@gmail.com>
2016-03-17 00:02:03 +0900

    Update yamanote.js

--- yamanote.js
+++ yamanote.js
@@ -1,11 +1,12 @@
 (function(ext) {
 
-  ext.b = function(name, callback) {
+  ext.b = function(cd, name, callback) {
     $.ajax({
       method: "GET",
       url: "http://www.ekidata.jp/api/l/11302.json",
       data: {
-        screen_name: name,
+        line_cd: cd,
+        line_name: name,
         count: 1
       },
       dataType: "json",
@@ -29,10 +30,10 @@
 
   var descriptor = {
     blocks: [
-      ['R', 'a', 'b', 'c'],
+       ['R', '都道府県', 'b', 'c', 'd'],
     ],
   };
 
-  ScratchExtensions.register('yamanote', descriptor, ext);
+  ScratchExtensions.register('都道府県', descriptor, ext);
 
 })({});
