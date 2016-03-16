ecfc2ea528e1abef2b18242db9d35d7c3b7e1833
MMirai <g121749@gmail.com>
2016-03-17 00:57:29 +0900

    Update station.js

--- station.js
+++ station.js
@@ -1,33 +1,24 @@
 (function(ext) {
+    // shutdown時に呼ばれる
+    ext._shutdown = function() {};
 
-  ext.b = function(cd, name, callback) {
-    $.ajax({
-      method: "GET",
-      url: " http://www.ekidata.jp/api/p/20.json",
-      dataType: "script",
-      success: function(data, dataType) {
-        alert("ok");
-       console.log("ok", xml.data); 
-      }
-        callback("No tweets found");
-      },
-      error: function(xhr, textStatus, error) {
-        console.log(error);
-        callback();
-      }
-    });
-  };
+    // statusを返してやる。デバイスとつながってない時とかここで色々返せる。
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
-  ext._getStatus = function() {
-    return { status:1, msg:'Ready' };
-  };
+    // blockが呼び出された時に呼ばれる関数を登録する。
+    // 下にあるdescriptorでブロックと関数のひも付けを行っている。
+    ext.do_domething = function(str) {
+    };
 
-  var descriptor = {
-    blocks: [
-       ['R', '駅', 'b', 'c', 'd'],
-    ],
-  };
+    // ブロックと関数のひも付け
+    var descriptor = {
+        blocks: [
+            [' ', 'do_something %s', 'do_something', 'sample text'],
+        ]
+    };
 
-  ScratchExtensions.register('駅', descriptor, ext);
-
+    // 最後にExtensionを登録する
+    ScratchExtensions.register('Simple extension', descriptor, ext);
 })({});
